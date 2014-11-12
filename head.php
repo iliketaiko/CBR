c37ac3272531b60e28daa357ef9656cffd89d484
iliketaiko <iliketaiko@gmail.com>
2014-11-12 15:54:31 +0800

    Revert "V1.0"
    This reverts commit 74a4fa36f5b9bd3bcbac3517b3ba61d08262b685.

--- head.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbserver = "127.0.0.1";
-$connect = mysql_connect($dbserver, "root", "2adiirxl") or die("Connection fail");
-mysql_select_db("cbrdatabase", $connect);
-mysql_query("SET NAMES UTF8"); 
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-session_unset(); 
-session_destroy();
-?>
-<script>
-location.href = "index.php";
-</script>
